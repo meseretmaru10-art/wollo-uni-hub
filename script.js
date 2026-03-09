28b6a9c3a9c48cfd93dc5920bc64519c33e57238
meseretmaru10-art <meseretmaru10@gmail.com>
2026-03-09 19:12:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,144 @@
-// KIOT Freshman Sections A-N Schedule Data
 const scheduleData = {
-    "A": { "Monday": "Math1041 (8:00-9:45) CR3201", "Tuesday": "ECEg1052 (10:00-11:45)", "Wednesday": "EmTe1012 (2:10-3:55)", "Thursday": "Anth1012 (8:00-9:45)", "Friday": "Hist1012 (8:00-9:45)" },
-    "B": { "Monday": "EmTe1012 (8:00-9:45) CR3202", "Tuesday": "Math1041 (10:00-11:45)", "Wednesday": "ECEg1052 (8:00-9:45)", "Thursday": "MCiE1012 (2:10-3:55)", "Friday": "FLEN1012 (2:10-3:55)" },
-    "C": { "Monday": "ECEg1052 (8:00-9:45) CR3203", "Tuesday": "Math1041 (8:00-9:45)", "Wednesday": "EmTe1012 (8:00-9:45)", "Thursday": "Math1041 (2:10-3:55)", "Friday": "FLEN1012 (8:00-9:45)" },
-    "D": { "Monday": "Math1041 (8:00-9:45) CR3204", "Tuesday": "MCiE1012 (10:00-11:45)", "Wednesday": "FLEN1012 (8:00-9:45)", "Thursday": "Math1041 (2:10-3:55)", "Friday": "Anth1012 (8:00-9:45)" },
-    "E": { "Monday": "ECEg1052 (2:10-3:55) CR3205", "Tuesday": "Math1041 (8:00-9:45)", "Wednesday": "EmTe1012 (10:00-11:45)", "Thursday": "Hist1012 (2:10-3:55)", "Friday": "Math1041 (8:00-9:45)" },
-    "F": { "Monday": "Math1041 (8:00-9:45) CR3206", "Tuesday": "Hist1012 (2:10-3:55)", "Wednesday": "EmTe1012 (8:00-9:45)", "Thursday": "Math1041 (2:10-3:55)", "Friday": "ECEg1052 (8:00-9:45)" },
-    "G": { "Monday": "Math1041 (8:00-9:45) CR3207", "Tuesday": "EmTe1012 (2:10-3:55)", "Wednesday": "FLEN1012 (10:00-11:45)", "Thursday": "MCiE1012 (8:00-9:45)", "Friday": "Anth1012 (2:10-3:55)" },
-    "H": { "Monday": "EmTe1012 (8:00-9:45) CR3208", "Tuesday": "Hist1012 (2:10-3:55)", "Wednesday": "Anth1012 (10:00-11:45)", "Thursday": "Math1041 (2:10-3:55)", "Friday": "MCiE1012 (8:00-9:45)" },
-    "I": { "Monday": "MCiE1012 (8:00-9:45) CR3209", "Tuesday": "EmTe1012 (10:00-11:45)", "Wednesday": "Math1041 (8:00-9:45)", "Thursday": "Hist1012 (2:10-3:55)", "Friday": "FLEN1012 (2:10-3:55)" },
-    "J": { "Monday": "EmTe1012 (8:00-9:45) CR3210", "Tuesday": "Math1041 (2:10-3:55)", "Wednesday": "FLEN1012 (2:10-3:55)", "Thursday": "Hist1012 (2:10-3:55)", "Friday": "Anth1012 (8:00-9:45)" },
-    "K": { "Monday": "Hist1012 (2:10-3:55) CR3211", "Tuesday": "EmTe1012 (8:00-9:45)", "Wednesday": "Math1041 (2:10-3:55)", "Thursday": "ECEg1052 (10:00-11:45)", "Friday": "ECEg1052 (8:00-9:45)" },
-    "L": { "Monday": "EmTe1012 (2:10-3:55) CR3212", "Tuesday": "Math1041 (2:10-3:55)", "Wednesday": "ECEg1052 (8:00-9:45)", "Thursday": "MCiE1012 (8:00-9:45)", "Friday": "Hist1012 (8:00-9:45)" },
-    "M": { "Monday": "ECEg1052 (8:00-9:45) CR3213", "Tuesday": "Math1041 (8:00-9:45)", "Wednesday": "Hist1012 (2:10-3:55)", "Thursday": "EmTe1012 (2:10-3:55)", "Friday": "Math1041 (8:00-9:45)" },
-    "N": { "Monday": "Math1041 (2:10-3:55) CR3214", "Tuesday": "MCiE1012 (10:00-11:45)", "Wednesday": "EmTe1012 (2:10-3:55)", "Thursday": "Math1041 (8:00-9:45)", "Friday": "Hist1012 (8:00-9:45)" }
+    "A": {
+        "Monday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3201" }, { course: "Hist1012", time: "8:50-9:30", room: "CR3201" }],
+        "Tuesday": [{ course: "ECEg1052", time: "4:00-4:50", room: "CR3201" }, { course: "MCiE1012", time: "4:55-5:45", room: "CR3201" }, { course: "FLEN1012", time: "8:50-9:30", room: "CR3201" }],
+        "Wednesday": [{ course: "Anth1012", time: "2:10-3:00", room: "CR3201" }, { course: "EmTe1012", time: "8:50-9:30", room: "CR3201" }],
+        "Thursday": [{ course: "Math1041", time: "9:35-11:10", room: "CR3201" }],
+        "Friday": [{ course: "EmTe1012", time: "2:10-3:00", room: "CR3201" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3201" }]
+    },
+    "B": {
+        "Monday": [{ course: "EmTe1012", time: "3:05-3:55", room: "CR3202" }, { course: "Math1041", time: "8:50-9:30", room: "CR3202" }],
+        "Tuesday": [{ course: "ECEg1052", time: "3:05-3:55", room: "CR3202" }, { course: "Math1041", time: "8:50-10:20", room: "CR3202" }],
+        "Wednesday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3202" }, { course: "Anth1012", time: "4:00-4:50", room: "CR3202" }, { course: "MCiE1012", time: "8:50-9:30", room: "CR3202" }],
+        "Thursday": [{ course: "Hist1012", time: "2:10-3:00", room: "CR3202" }, { course: "FLEN1012", time: "9:35-10:20", room: "CR3202" }],
+        "Friday": []
+    },
+    "C": {
+        "Monday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3203" }, { course: "Anth1012", time: "3:05-3:55", room: "CR3203" }, { course: "EmTe1012", time: "9:35-10:20", room: "CR3203" }],
+        "Tuesday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3203" }, { course: "Hist1012", time: "8:50-9:30", room: "CR3203" }],
+        "Wednesday": [{ course: "EmTe1012", time: "3:05-3:55", room: "CR3203" }, { course: "MCiE1012", time: "9:35-10:20", room: "CR3203" }],
+        "Thursday": [{ course: "Math1041", time: "4:00-4:50", room: "CR3203" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3203" }],
+        "Friday": [{ course: "FLEN1012", time: "2:10-3:55", room: "CR3203" }]
+    },
+    "D": {
+        "Monday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3204" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3204" }],
+        "Tuesday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3204" }, { course: "MCiE1012", time: "3:05-3:55", room: "CR3204" }],
+        "Wednesday": [{ course: "FLEN1012", time: "2:10-3:55", room: "CR3204" }, { course: "EmTe1012", time: "8:50-9:30", room: "CR3204" }],
+        "Thursday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3204" }, { course: "Math1041", time: "9:35-11:10", room: "CR3204" }],
+        "Friday": [{ course: "Anth1012", time: "2:10-3:00", room: "CR3204" }, { course: "EmTe1012", time: "3:05-3:55", room: "CR3204" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3204" }]
+    },
+    "E": {
+        "Monday": [{ course: "ECEg1052", time: "9:35-11:10", room: "CR3205" }],
+        "Tuesday": [{ course: "Math1041", time: "3:05-3:55", room: "CR3205" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3205" }],
+        "Wednesday": [{ course: "Anth1012", time: "2:10-3:00", room: "CR3205" }, { course: "EmTe1012", time: "3:05-3:55", room: "CR3205" }, { course: "FLEN1012", time: "8:50-10:20", room: "CR3205" }],
+        "Thursday": [{ course: "MCiE1012", time: "2:10-3:00", room: "CR3205" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3205" }],
+        "Friday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3205" }, { course: "EmTe1012", time: "9:35-10:20", room: "CR3205" }]
+    },
+    "F": {
+        "Monday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3206" }, { course: "EmTe1012", time: "8:50-10:20", room: "CR3206" }],
+        "Tuesday": [{ course: "Anth1012", time: "2:10-3:00", room: "CR3206" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3206" }],
+        "Wednesday": [{ course: "EmTe1012", time: "2:10-3:55", room: "CR3206" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3206" }],
+        "Thursday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3206" }, { course: "FLEN1012", time: "8:50-10:20", room: "CR3206" }],
+        "Friday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3206" }, { course: "MCiE1012", time: "3:05-3:55", room: "CR3206" }]
+    },
+    "G": {
+        "Monday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3207" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3207" }],
+        "Tuesday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3207" }, { course: "EmTe1012", time: "8:50-10:20", room: "CR3207" }],
+        "Wednesday": [{ course: "FLEN1012", time: "2:10-3:55", room: "CR3207" }],
+        "Thursday": [{ course: "MCiE1012", time: "2:10-3:00", room: "CR3207" }, { course: "EmTe1012", time: "4:00-4:50", room: "CR3207" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3207" }],
+        "Friday": [{ course: "Anth1012", time: "4:00-4:50", room: "CR3207" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3207" }]
+    },
+    "H": {
+        "Monday": [{ course: "EmTe1012", time: "2:10-3:55", room: "CR3208" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3208" }],
+        "Tuesday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3208" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3208" }],
+        "Wednesday": [{ course: "EmTe1012", time: "2:10-3:00", room: "CR3208" }, { course: "Anth1012", time: "4:00-4:50", room: "CR3208" }],
+        "Thursday": [{ course: "FLEN1012", time: "2:10-3:55", room: "CR3208" }, { course: "Math1041", time: "8:50-10:20", room: "CR3208" }],
+        "Friday": [{ course: "MCiE1012", time: "2:10-3:00", room: "CR3208" }, { course: "Math1041", time: "3:05-3:55", room: "CR3208" }]
+    },
+    "I": {
+        "Monday": [{ course: "MCiE1012", time: "2:10-3:55", room: "CR3209" }, { course: "Anth1012", time: "8:50-9:30", room: "CR3209" }],
+        "Tuesday": [{ course: "EmTe1012", time: "4:00-4:50", room: "CR3209" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3209" }],
+        "Wednesday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3209" }, { course: "ECEg1052", time: "9:35-10:20", room: "CR3209" }],
+        "Thursday": [{ course: "EmTe1012", time: "3:05-3:55", room: "CR3209" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3209" }],
+        "Friday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3209" }, { course: "FLEN1012", time: "8:50-10:20", room: "CR3209" }]
+    },
+    "J": {
+        "Monday": [{ course: "EmTe1012", time: "3:05-4:50", room: "CR3210" }, { course: "Math1041", time: "8:50-9:30", room: "CR3210" }],
+        "Tuesday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3210" }],
+        "Wednesday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3210" }, { course: "EmTe1012", time: "4:00-4:50", room: "CR3210" }, { course: "FLEN1012", time: "8:50-10:20", room: "CR3210" }],
+        "Thursday": [{ course: "Hist1012", time: "2:10-3:55", room: "CR3210" }, { course: "ECEg1052", time: "8:50-10:20", room: "CR3210" }],
+        "Friday": [{ course: "Anth1012", time: "2:10-3:55", room: "CR3210" }, { course: "MCiE1012", time: "4:00-4:50", room: "CR3210" }]
+    },
+    "K": {
+        "Monday": [{ course: "Anth1012", time: "2:10-3:00", room: "CR3211" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3211" }],
+        "Tuesday": [{ course: "EmTe1012", time: "3:05-4:50", room: "CR3211" }, { course: "EmTe1012", time: "7:50-8:40", room: "CR3211" }, { course: "MCiE1012", time: "8:50-9:30", room: "CR3211" }],
+        "Wednesday": [{ course: "FLEN1012", time: "2:10-3:55", room: "CR3211" }, { course: "Math1041", time: "8:50-10:20", room: "CR3211" }],
+        "Thursday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3211" }, { course: "ECEg1052", time: "4:00-4:50", room: "CR3211" }],
+        "Friday": [{ course: "ECEg1052", time: "2:10-3:55", room: "CR3211" }]
+    },
+    "L": {
+        "Monday": [{ course: "EmTe1012", time: "7:50-8:40", room: "CR3212" }],
+        "Tuesday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3212" }, { course: "Anth1012", time: "4:00-4:50", room: "CR3212" }, { course: "Math1041", time: "8:50-10:20", room: "CR3212" }],
+        "Wednesday": [{ course: "ECEg1052", time: "2:10-3:55", room: "CR3212" }, { course: "FLEN1012", time: "8:50-10:20", room: "CR3212" }],
+        "Thursday": [{ course: "MCiE1012", time: "2:10-3:00", room: "CR3212" }, { course: "EmTe1012", time: "3:05-3:55", room: "CR3212" }],
+        "Friday": [{ course: "Hist1012", time: "2:10-3:55", room: "CR3212" }, { course: "Math1041", time: "8:50-10:20", room: "CR3212" }]
+    },
+    "M": {
+        "Monday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3213" }, { course: "MCiE1012", time: "3:05-3:55", room: "CR3213" }],
+        "Tuesday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3213" }, { course: "EmTe1012", time: "4:00-4:50", room: "CR3213" }],
+        "Wednesday": [{ course: "FLEN1012", time: "2:10-3:55", room: "CR3213" }, { course: "Hist1012", time: "8:50-10:20", room: "CR3213" }],
+        "Thursday": [{ course: "ECEg1052", time: "2:10-3:55", room: "CR3213" }, { course: "EmTe1012", time: "8:50-9:30", room: "CR3213" }],
+        "Friday": [{ course: "Math1041", time: "2:10-3:55", room: "CR3213" }, { course: "Anth1012", time: "8:50-9:30", room: "CR3213" }]
+    },
+    "N": {
+        "Monday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3214" }, { course: "Math1041", time: "8:50-9:30", room: "CR3214" }],
+        "Tuesday": [{ course: "ECEg1052", time: "2:10-3:00", room: "CR3214" }, { course: "MCiE1012", time: "3:05-3:55", room: "CR3214" }],
+        "Wednesday": [{ course: "Anth1012", time: "2:10-3:00", room: "CR3214" }, { course: "EmTe1012", time: "8:50-10:20", room: "CR3214" }],
+        "Thursday": [{ course: "Math1041", time: "2:10-3:00", room: "CR3214" }, { course: "EmTe1012", time: "3:05-3:55", room: "CR3214" }],
+        "Friday": [{ course: "Hist1012", time: "2:10-3:55", room: "CR3214" }, { course: "FLEN1012", time: "8:50-10:20", room: "CR3214" }]
+    }
 };
 
-const sectionSelect = document.getElementById('sectionSelect');
-const dayButtons = document.querySelectorAll('.day-btn');
-const displayArea = document.getElementById('scheduleDisplay');
-let selectedDay = "";
+let selectedSection = "";
 
-dayButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        dayButtons.forEach(btn => btn.classList.remove('active'));
-        button.classList.add('active');
-        selectedDay = button.innerText;
-        showSchedule();
+document.getElementById('sectionSelect').addEventListener('change', (e) => {
+    selectedSection = e.target.value;
+    updateDisplay("");
+});
+
+document.querySelectorAll('.day-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (!selectedSection) {
+            alert("እባክዎን መጀመሪያ ሴክሽን ይምረጡ!");
+            return;
+        }
+        updateDisplay(btn.innerText);
     });
 });
 
-sectionSelect.addEventListener('change', showSchedule);
-
-function showSchedule() {
-    const section = sectionSelect.value;
-    if (section && selectedDay) {
-        const result = scheduleData[section] && scheduleData[section][selectedDay] 
-            ? scheduleData[section][selectedDay] 
-            : "ምንም የትምህርት ፕሮግራም የለም።";
-        displayArea.innerHTML = `<div class="card"><h3>Section ${section} - ${selectedDay}</h3><p>${result}</p></div>`;
+function updateDisplay(day) {
+    const display = document.getElementById('scheduleDisplay');
+    if (!day) {
+        display.innerHTML = `<p class='placeholder-text'>Section ${selectedSection} ተመርጧል። አሁን ቀን ይምረጡ።</p>`;
+        return;
     }
+
+    const sessions = scheduleData[selectedSection][day];
+    let html = `<h3>Section ${selectedSection} - ${day}</h3>`;
+
+    if (sessions.length === 0) {
+        html += `<p class='no-class'>ለዚህ ቀን ምንም ክፍለ ጊዜ የለም።</p>`;
+    } else {
+        sessions.forEach(s => {
+            html += `
+                <div class="schedule-item">
+                    <div class="course-info">
+                        <span class="course-name">${s.course}</span>
+                        <span class="room-name">Room: ${s.room}</span>
+                    </div>
+                    <div class="time-info">${s.time}</div>
+                </div>`;
+        });
+    }
+    display.innerHTML = html;
 }
