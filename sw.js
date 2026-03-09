86eca5f644ad6cb3a3204ee4a53216d0696c4d05
meseretmaru10-art <meseretmaru10@gmail.com>
2026-03-10 00:55:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,25 @@
-const CACHE_NAME = 'wollo-uni-hub-v1';
-const urlsToCache = [
+const CACHE_NAME = 'kiot-hub-v1';
+const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
+  './manifest.json',
   './icon.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(res => {
+      return res || fetch(evt.request);
+    })
   );
 });
