32c5c9789c4619d40a9f1a1b37762754f25a0020
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-15 15:15:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,45 @@
-const CACHE_NAME = 'kiot-hub-v1';
+const cacheName = 'wollo-uni-v2';
 const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json',
-  './icon.png'
+  './icon.png',
+  './manifest.json'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+// 1. ፋይሎቹን በስልኩ ሜሞሪ ላይ ለመጫን (Install)
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets for offline use...');
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => {
-      return res || fetch(evt.request);
+// 2. አሮጌ መረጃዎችን ለማጥፋት (Activate)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 3. ኢንተርኔት በሌለበት ጊዜ መረጃውን ለማቅረብ (Fetch)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    }).catch(() => {
+      // ፋይሉ በካሽ ውስጥ ከሌለ እና ኢንተርኔት ከጠፋ
+      if (e.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
     })
   );
 });
