c7ab4f0cea0966335038dd25c0d130b7f6009438
kiot-pre-engineering-module <meseretmaru10@gmail.com>
2026-03-25 09:52:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,4 @@
-const CACHE_NAME = 'wollo-uni-hub-v6';
-
-// ያለ ዳታ እንዲከፈቱ የሚቀመጡ ዋና ዋና ፋይሎች ዝርዝር
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './style.css',
-  './script.js',
-  './icon.png'
-];
-
-// ፋይሎቹን መጀመሪያ ሲከፈት በስልኩ ላይ መጫን
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching main assets for offline use...');
-      return cache.addAll(assets);
-    })
-  );
-});
-
-// አሮጌ ካሽ ካለ አዲሱን ስሪት ለማስገባት ማጽዳት
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// ዳታ በሌለበት ጊዜ መረጃን ከስልኩ ካሽ መጥራት
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
-});
+const CACHE_NAME = 'kiot-cache-v1';
+const assets = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
